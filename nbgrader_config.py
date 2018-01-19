cffdd04665628c4f9eb03d02d16ba85e9eead56b
Matthew Turk <matthewturk@gmail.com>
2018-01-19 16:29:22 -0600

    initial import

---
+++ nbgrader_config.py
@@ -0,0 +1,6 @@
+import os
+c = get_config()
+
+c.Exchange.course_id = 'sp18-is590dv'
+c.Exchange.root = '/home/shared/sp18-is590dv/sp18-is590dv/exchange'
+c.CourseDirectory.root = os.path.expanduser("~/shared/sp18-is590dv/sp18-is590dv/")
