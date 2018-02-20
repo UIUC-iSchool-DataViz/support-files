2d703c626882087a72b88d39163d483f2cd1f970
Matthew Turk <matthewturk@gmail.com>
2018-02-20 12:07:41 -0500

    Remove trailing slash

--- nbgrader_config.py
+++ nbgrader_config.py
@@ -3,4 +3,4 @@ c = get_config()
 
 c.Exchange.course_id = 'sp18-is590dv'
 c.Exchange.root = '/home/shared/sp18-is590dv/sp18-is590dv/exchange'
-c.CourseDirectory.root = os.path.expanduser("~/shared/sp18-is590dv/sp18-is590dv/")
+c.CourseDirectory.root = os.path.expanduser("~/shared/sp18-is590dv/sp18-is590dv")
